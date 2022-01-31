4828fe02f5b95b147c4f6afda94acccc54a72b19
Youssef Girgeis <youssefgirgeis@gmail.com>
2022-01-30 22:08:56 -0500

    set up a post end point to get data from an api

--- dist/main.js
+++ dist/main.js
@@ -865,11 +865,16 @@ function handleSubmit(event) {
   event.preventDefault();
 
   // check what text was put into the form field
-  let formText = document.getElementById('name').value;
-  Client.checkForName(formText);
+  let url = document.getElementById('name').value;
+  Client.checkForName(url);
 
   console.log('::: Form Submitted :::');
-  fetch('http://localhost:8080/test')
+  fetch('/api', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ url }),
+  })
     .then((res) => res.json())
     .then(function (res) {
       document.getElementById('results').innerHTML = res.message;
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,24 @@
 const dotenv = require('dotenv');
+const path = require('path');
+var bodyParser = require('body-parser');
 dotenv.config();
 const express = require('express');
-const app = express();
 
 const API_KEY = process.env.API_KEY;
-console.log(API_KEY);
+const app = express();
+
+app.use(express.static('dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile('dist/index.html');
+  //res.sendFile(path.resolve('src/client/views/index.html'));
+});
+
+app.post('/api', (req, res) => {
+  console.log('URL = ', req.body.url);
+});
 
 app.listen(8081, () => {
   console.log('Listening on port 8081');
